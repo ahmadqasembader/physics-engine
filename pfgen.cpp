a744ff7d077b528823701348c3deed034950ec35
ahmadbader <ahmadbader245@gmail.com>
2023-03-04 23:17:22 +0200

    force registry class implementation

--- pfgen.cpp
+++ pfgen.cpp
@@ -12,10 +12,20 @@ using Gorgon::Physics::ParticleForceRegistry;
 
 void ParticleForceRegistry::Add(Particle * particle, ParticleForceGenerator *fg)
 {
-
+    // creates a new registry, and assign to it the particle and the fg
+    // and then push back to the list of registrations
+    ParticleForceRegistry::ParticleRegistration registration;
+    registration.particle = particle;
+    registration.fg = fg;
+    registrations.push_back(registration);
 }
 
 void ParticleForceRegistry::UpdateForces(double time)
 {
-
+    //loop over all the fg and update all the forces
+    Registry::iterator itr = registrations.begin();
+    for(; itr != registrations.end(); itr++)
+    {   
+        itr->fg->UpdateForce(itr->particle, time);
+    }
 }
